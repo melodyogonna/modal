ca42a9f9a78d610b50014cb298508e471e32f768
Melody Daniel <melodyogonna@gmail.com>
2024-01-09 20:42:05 +0100

    Configure wgpu surface

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use winit::{
     window::WindowBuilder,
 };
 
-fn main() {
+mod state;
+
+async fn run() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Modal Editor")
         .build(&event_loop)
         .unwrap();
+    let uistate = state::UIGraphicsState::new(window).await;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -17,8 +20,12 @@ fn main() {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            } if window_id == uistate.window.id() => *control_flow = ControlFlow::Exit,
             _ => (),
         }
     });
+}
+
+fn main() {
+    pollster::block_on(run())
 }
---
+++ src/state.rs
@@ -0,0 +1,74 @@
+use winit::window::Window;
+
+pub struct UIGraphicsState {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    size: winit::dpi::PhysicalSize<u32>,
+    config: wgpu::SurfaceConfiguration,
+    pub window: Window,
+}
+
+impl UIGraphicsState {
+    pub async fn new(window: Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptionsBase {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let surface_capability = surface.get_capabilities(&adapter);
+        let surface_format = surface_capability
+            .formats
+            .iter()
+            .copied()
+            .filter(|f| f.is_srgb())
+            .next()
+            .unwrap_or(surface_capability.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_capability.present_modes[0],
+            alpha_mode: surface_capability.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window,
+        }
+    }
+}
